70de099c1051d71805acdcc36ca10e2a260aca46
tysonv7 <tyson_v7@hotmail.com>
2016-09-25 15:06:17 +0800

    Can generate HTTP Request Message

--- tcpcrawler.cpp
+++ tcpcrawler.cpp
@@ -12,14 +12,27 @@
 
 using namespace std;
 
-int main (int argc, char* argv[]) {
+string generateHttpRequest(char *path) {
+	string requestMessage = "GET ";
+	requestMessage.append(path); //path is the hostURL
+	requestMessage.append(" HTTP/1.0\r\n");
+	requestMessage.append("Host: ");
+	requestMessage.append("\r\n");
+	requestMessage.append("Accept: text/*, */*;q=0/9\r\n");
+	requestMessage.append("Connection: close\r\n\r\n");
+	return requestMessage;
+}
+
+int connect(char* website) {
 	int sock, byteReceived;
 	char dataSent[1024];
 	char dataReceived[1024];
 	struct hostent* host;
 	struct sockaddr_in server_addr;
 	
-	host = gethostbyname(argv[1]);
+	host = gethostbyname(website);
+	
+	cout << "Try to visit: " << website << endl;
 	
 	//Creating a socket Structure
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -40,4 +53,11 @@ int main (int argc, char* argv[]) {
 	
 	cout << "Connected to " << inet_ntoa(server_addr.sin_addr) << " " << ntohs(server_addr.sin_port) << endl;
 	
+	string requestMessage = generateHttpRequest(website);
+	cout << requestMessage << endl;
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	connect(argv[1]);
 }
